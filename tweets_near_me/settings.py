a091824348fc40d202dc299026e68bcccdc72b3a
Mehtab Singh Edhan <37650533+mehtabedhan@users.noreply.github.com>
2021-05-06 01:31:55 +0530

    s

--- tweets_near_me/settings.py
+++ tweets_near_me/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
+import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ye1l86165_4%y*k6cean9u23pv8q$l*^#^-(m#yb21ae#a*)z='
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.115', '192.168.0.119', '172.16.74.2', '127.0.0.1']
+# ALLOWED_HOSTS = ['192.168.0.115', '192.168.0.119', '172.16.74.2', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
